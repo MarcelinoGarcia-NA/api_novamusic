abfd71179babc688a1184bef58642852188503de
MarcelinoGarcia-NA <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2022-01-29 20:06:16 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const cors= require("cors");
 const PORT = process.env.PORT || 3000;
-mongoose.connect("mongodb+srv://marcelino-na:marcelino-na@cluster0.wvw2d.mongodb.net/NovaMusicBD?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://mvitor97:marcelino-na@cluster0.wvw2d.mongodb.net/NovaMusicBD?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true,
