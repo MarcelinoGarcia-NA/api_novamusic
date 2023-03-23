19fcc1de3205cf466838c829093d07e6ac25427e
Marcelino Garcia <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2023-03-23 15:02:49 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const cors= require("cors");
-const PORT = process.env.PORT || 3000;
+
 mongoose.connect("mongodb+srv://mvitor97:marcelino-na@cluster0.wvw2d.mongodb.net/NovaMusicBD?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
